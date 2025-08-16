407c87d233be10fecc1ae6db76a7948ba7ea2065
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-08-16 04:32:57 -0700

    create "ToDoList.jsx" and updated the index.css
    NOTE: I created "ToDoList.jsx", added the To Do List default set up function and export to "app.jsx" & updated the "index.css"  by adding the CSS reset, background-color and test-color.

--- to do list/src/App.jsx
+++ to do list/src/App.jsx
@@ -1,10 +1,11 @@
+import ToDoList from "./ToDoList"
 
 function App() {
 
 
   return (
     <>
-    
+    <ToDoList/>
     </>
   )
 }
---
+++ to do list/src/ToDoList.jsx
@@ -0,0 +1,10 @@
+
+
+function ToDoList(){
+
+
+
+    return(<></>);
+}
+
+export default ToDoList
